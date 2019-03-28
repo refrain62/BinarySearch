66efaae5bc0b95c701b53b372ddb9a836878a497
refrain <nao_h13@hotmail.com>
2019-03-29 00:34:12 +0900

    タブ文字を空白に変更

--- src/main/java/search/binary/App.java
+++ src/main/java/search/binary/App.java
@@ -6,118 +6,118 @@ import java.io.InputStreamReader;
 
 public class App
 {
-	public static void main( String[] args )
-	{
-		BinarySearch binarySearch = BinarySearch.getInstance();
-		
-		int result = BinarySearch.C_NOT_FOUND;
-		int searchVal = -999;
-		
-		// 探索データの定義
-		// ※対象データはクイックソートなどで事前に昇順に並んでいる前提
-		int[] searchData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  public static void main( String[] args )
+  {
+    BinarySearch binarySearch = BinarySearch.getInstance();
+    
+    int result = BinarySearch.C_NOT_FOUND;
+    int searchVal = -999;
+    
+    // 探索データの定義
+    // ※対象データはクイックソートなどで事前に昇順に並んでいる前提
+    int[] searchData = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-		System.out.println("Binary Search App Start!");
-		
-		// 検索データの出力
-		outputSearchData(
-						searchData
-						);
-		
-		// キー入力（数値として）
-		searchVal = inputKeyValue();
+    System.out.println("Binary Search App Start!");
+    
+    // 検索データの出力
+    outputSearchData(
+            searchData
+            );
+    
+    // キー入力（数値として）
+    searchVal = inputKeyValue();
 
-		// 探索（とりあえず開始時は全範囲指定）
-		result = binarySearch.execute(
-										searchData,
-										0,
-										searchData.length - 1,
-										searchVal
-										);
-		
-		// 結果の出力
-		outputResult(
-							result,
-							BinarySearch.C_NOT_FOUND
-							);
-	}
+    // 探索（とりあえず開始時は全範囲指定）
+    result = binarySearch.execute(
+                    searchData,
+                    0,
+                    searchData.length - 1,
+                    searchVal
+                    );
+    
+    // 結果の出力
+    outputResult(
+              result,
+              BinarySearch.C_NOT_FOUND
+              );
+  }
 
-	/**
-	 * 検索データの出力
-	 * @param argSearchData
-	 * @return
-	 */
-	private static void outputSearchData(
-										int[] argSearchData
-										)
-	{
-		String strDelimiter = "";
-		
-		System.out.print("Array Data is [");
+  /**
+   * 検索データの出力
+   * @param argSearchData
+   * @return
+   */
+  private static void outputSearchData(
+                    int[] argSearchData
+                    )
+  {
+    String strDelimiter = "";
+    
+    System.out.print("Array Data is [");
 
-		strDelimiter = "";
-		
-		for( int val : argSearchData )
-		{
-			System.out.print( strDelimiter + val );
-			
-			strDelimiter = ", ";
-		}
-		
-		System.out.println("]");
-	}
-	
-	/**
-	 *  キー入力（数値として）
-	 * @return
-	 */
-	private static int inputKeyValue()
-	{
-	    String input = null;
-	    int number = 0;
+    strDelimiter = "";
+    
+    for( int val : argSearchData )
+    {
+      System.out.print( strDelimiter + val );
+      
+      strDelimiter = ", ";
+    }
+    
+    System.out.println("]");
+  }
+  
+  /**
+   *  キー入力（数値として）
+   * @return
+   */
+  private static int inputKeyValue()
+  {
+      String input = null;
+      int number = 0;
 
-		System.out.println("What is the search value of array data？");
-		
-	    try
-	    {
-	        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
-	        
-	        input = bufferedReader.readLine();
-	        
-	        number = Integer.parseInt(input);
-	    }
-	    catch( NumberFormatException ex )
-	    {
-	       System.out.println("Not a number !");
-	    }
-	    catch( IOException e )
-	    {
-	        e.printStackTrace();
-	    }
-	    
-	    return number;
-	}
-	
-	/**
-	 *  結果の出力
-	 * @param argResult
-	 * @param argNotFoundValue
-	 */
-	private static void outputResult(
-								int argResult,
-								int argNotFoundValue
-							)
-	{
-		// 探索結果NG
-		if(		argNotFoundValue == argResult
-			)
-		{
-			System.out.println("Data Not Found.");
-		}
-		// 探索OK
-		else
-		{
-			System.out.println("Data Found. Array index is " + (argResult + 1) );	
-		}
-	}
+    System.out.println("What is the search value of array data？");
+    
+      try
+      {
+          BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+          
+          input = bufferedReader.readLine();
+          
+          number = Integer.parseInt(input);
+      }
+      catch( NumberFormatException ex )
+      {
+         System.out.println("Not a number !");
+      }
+      catch( IOException e )
+      {
+          e.printStackTrace();
+      }
+      
+      return number;
+  }
+  
+  /**
+   *  結果の出力
+   * @param argResult
+   * @param argNotFoundValue
+   */
+  private static void outputResult(
+                int argResult,
+                int argNotFoundValue
+              )
+  {
+    // 探索結果NG
+    if(   argNotFoundValue == argResult
+      )
+    {
+      System.out.println("Data Not Found.");
+    }
+    // 探索OK
+    else
+    {
+      System.out.println("Data Found. Array index is " + (argResult + 1) ); 
+    }
+  }
 }
--- src/main/java/search/binary/BinarySearch.java
+++ src/main/java/search/binary/BinarySearch.java
@@ -12,81 +12,81 @@ package search.binary;
 
 public class BinarySearch
 {
-	// Singleton用
-	private static BinarySearch binarySearch = new BinarySearch(); 
-	
-	// 見つからなかった場合の戻り値
-	public static int C_NOT_FOUND = -1;
-	
-	// 外部からインスタンス化できないようにコンストラクタをprivate化
-	private BinarySearch()
-	{	
-	}
-	
-	// インスタンスを返す
-	public static BinarySearch getInstance()
-	{
-		return binarySearch;
-	}
-	
-	/**
-	 *  探索実行
-	 * @param argData				探索データ群
-	 * @param argSearchIndexFrom	探索開始位置
-	 * @param argSearchIndexTo		探索終了位置
-	 * @param argVal				目的データ
-	 * @return
-	 */
-	public int execute(
-						int[] argData,
-						int argSearchIndexFrom,
-						int argSearchIndexTo,
-						int argVal
-						)
-	{
-		int middleIndex = 0;
-		
-		if(		argData != null
-			&&	argSearchIndexFrom <= argSearchIndexTo
-			)
-		{
-			// 中央のインデックスを算出
-			middleIndex = argSearchIndexFrom + ((argSearchIndexTo - argSearchIndexFrom) / 2);
+  // Singleton用
+  private static BinarySearch binarySearch = new BinarySearch(); 
+  
+  // 見つからなかった場合の戻り値
+  public static int C_NOT_FOUND = -1;
+  
+  // 外部からインスタンス化できないようにコンストラクタをprivate化
+  private BinarySearch()
+  { 
+  }
+  
+  // インスタンスを返す
+  public static BinarySearch getInstance()
+  {
+    return binarySearch;
+  }
+  
+  /**
+   *  探索実行
+   * @param argData       探索データ群
+   * @param argSearchIndexFrom  探索開始位置
+   * @param argSearchIndexTo    探索終了位置
+   * @param argVal        目的データ
+   * @return
+   */
+  public int execute(
+            int[] argData,
+            int argSearchIndexFrom,
+            int argSearchIndexTo,
+            int argVal
+            )
+  {
+    int middleIndex = 0;
+    
+    if(   argData != null
+      &&  argSearchIndexFrom <= argSearchIndexTo
+      )
+    {
+      // 中央のインデックスを算出
+      middleIndex = argSearchIndexFrom + ((argSearchIndexTo - argSearchIndexFrom) / 2);
 
-			// 情報をログに出力
-			System.out.println( "from : " + argSearchIndexFrom + " to : " + argSearchIndexTo + " middle : " + middleIndex + " val: " + argData[ middleIndex ] );
-			
-			// 中央のインデックスの値が一致した場合は終了
-			if(		argData[ middleIndex ] == argVal
-				)
-			{
-				return middleIndex;
-			}
-			
-			// 目的の値が小さい場合
-			if(		argData[ middleIndex ] > argVal
-				)
-			{
-				// 前半の要素から再帰探索
-				return this.execute(
-									argData,
-									argSearchIndexFrom,
-									middleIndex - 1,
-									argVal
-									);
-			}
-			else
-			{
-				// 後半の要素から再帰探索
-				return this.execute(
-									argData,
-									middleIndex + 1,
-									argSearchIndexTo,
-									argVal
-									);
-			}
-		}
-		
-		return C_NOT_FOUND;
-	}
+      // 情報をログに出力
+      System.out.println( "from : " + argSearchIndexFrom + " to : " + argSearchIndexTo + " middle : " + middleIndex + " val: " + argData[ middleIndex ] );
+      
+      // 中央のインデックスの値が一致した場合は終了
+      if(   argData[ middleIndex ] == argVal
+        )
+      {
+        return middleIndex;
+      }
+      
+      // 目的の値が小さい場合
+      if(   argData[ middleIndex ] > argVal
+        )
+      {
+        // 前半の要素から再帰探索
+        return this.execute(
+                  argData,
+                  argSearchIndexFrom,
+                  middleIndex - 1,
+                  argVal
+                  );
+      }
+      else
+      {
+        // 後半の要素から再帰探索
+        return this.execute(
+                  argData,
+                  middleIndex + 1,
+                  argSearchIndexTo,
+                  argVal
+                  );
+      }
+    }
+    
+    return C_NOT_FOUND;
+  }
 }
